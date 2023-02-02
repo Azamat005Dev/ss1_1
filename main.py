d3d1fecb469c9a00c037e5e90b4673aab755ed03
Azamat005Dev <124263704+Azamat005Dev@users.noreply.github.com>
2023-02-02 17:39:10 +0500

    main.py
    test

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-import tkinter
-import webbrowser
-from tkinter import messagebox
-
-window = tkinter.Tk()
-windaw = title('My First App')
-windaw = geomerty('640*480')
-windaw = resizbale(False,False)
-windaw = config(bg= '#bd9491')
-
-
+a = int(input('Birinshi sandi kirgizin')
+b = int(input('Ekinshi sandi kirgizin')
+print(a+b)
