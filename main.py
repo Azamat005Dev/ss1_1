960c1e1754650f968e619fca85ddb4cdef6970d3
Azamat005Dev <qalmuratovazamat14@gmail.com>
2023-02-02 17:24:30 +0500

    first

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter
+import webbrowser
+from tkinter import messagebox
+
+window = tkinter.Tk()
+windaw = title('My First App')
+windaw = geomerty('640*480')
+windaw = resizbale(False,False)
+windaw = config(bg= '#bd9491')
+
+
